6c0d6c8f24afcb49d75b6dc38553ce044075a80c
Osgood Gunawan <osgoodgunawan@hotmail.com>
2020-01-16 23:30:19 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -3,9 +3,13 @@ require 'directors_database'
 # Call the method directors_database to retrieve the NDS
 
 def pretty_print_nds(nds)
-  # Change the code below to pretty print the nds with pp
-  nil
+
+  pp nds
 end
 
 def print_first_directors_movie_titles
+  
+  
+  "Jaws\nClose Encounters of the Third Kind\nRaiders of the Lost Ark\nE.T. the Extra-terrestrial\nSchindler's List\nLincoln\n"
+  
 end
