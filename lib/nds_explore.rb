ac0183914b8cb6229f45a9479c0f126213b8e845
Osgood Gunawan <osgoodgunawan@hotmail.com>
2020-01-16 23:31:14 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -1,13 +1,15 @@
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 require 'directors_database'
 # Call the method directors_database to retrieve the NDS
+
 require 'pp'
+# to retrieve pp library
 
 def pretty_print_nds(nds)
-
   pp nds
 end
 
+print_first_directors_movie_titles(directors_database)
 def print_first_directors_movie_titles
   
   
